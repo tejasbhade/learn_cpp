c50353e8c765e6aba711653f53a4df512c97f257
tejasbhade <tejas.bhade619@gmail.com>
2026-01-14 22:46:32 -0500

    two_sum

---
+++ two_sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> result = solution.twoSum(nums, target);
+    
+    cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
+    
+    return 0;
+}
